8d20204fec2b82eb23a132b6feb90d9d86ce0950
Cippy99 <antocipani@gmail.com>
2023-05-13 14:56:12 +0200

    Added Fragments

--- app/src/main/java/com/example/groceriesinventory/MainActivity.kt
+++ app/src/main/java/com/example/groceriesinventory/MainActivity.kt
@@ -2,17 +2,28 @@ package com.example.groceriesinventory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.groceriesinventory.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = binding.bottomNavigationView
+        bottomNavigationView.setupWithNavController(navController)
 
+        val view = binding.root
         setContentView(view)
     }
+
 }
