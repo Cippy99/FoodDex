ada110105179c8b51bbb80957a128f0aafb33813
edoofra <edoardo.fra.ef@gmail.com>
2023-07-24 17:51:00 +0200

    create Recipe and start working on RecipesFragment

--- app/src/main/java/com/example/fooddex/Recipe.kt
+++ app/src/main/java/com/example/fooddex/Recipe.kt
@@ -1,4 +1,20 @@
 package com.example.fooddex
 
-class Recipe {
+class Recipe() {
+    var id = "0"
+    var name: String = ""
+    var imgRef: String = ""
+    var category: String = ""
+    var nOfPerson: Int = 0
+    var products: MutableList<Product>? = null
+
+    constructor(id: String, name: String, imgRef: String, category: String,
+                nOfPerson: Int, products: MutableList<Product>) : this() {
+        this.name = name
+        this.id = id
+        this.imgRef = imgRef
+        this.category = category
+        this.nOfPerson = nOfPerson
+        this.products = products
+    }
 }
