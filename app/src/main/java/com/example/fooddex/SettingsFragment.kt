5803f4eb78e291e1cb9a3939f446a46fb83553e8
Cippy99 <32038799+Cippy99@users.noreply.github.com>
2023-08-22 11:57:50 +0200

    Show logged user in settings

--- app/src/main/java/com/example/fooddex/SettingsFragment.kt
+++ app/src/main/java/com/example/fooddex/SettingsFragment.kt
@@ -6,11 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.fooddex.databinding.FragmentSettingsBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -19,6 +21,7 @@ class SettingsFragment : Fragment() {
 
     private var _binding: FragmentSettingsBinding? = null
     private lateinit var auth: FirebaseAuth
+    private lateinit var dbRef: DatabaseReference
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -33,6 +36,10 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         auth = Firebase.auth
+        dbRef = Firebase.database.reference
+
+        retireveAndSetUsername()
+
         binding.btnLogout.setOnClickListener {
             val intent = Intent(activity, Login::class.java)
             auth.signOut()
@@ -43,7 +50,6 @@ class SettingsFragment : Fragment() {
 
         binding.btnFamily.setOnClickListener {
             //Start Family Setting Activity
-            val dbRef = Firebase.database.reference
 
             val userId = auth.currentUser?.uid!!
             var familyId: String? = null
@@ -70,6 +76,31 @@ class SettingsFragment : Fragment() {
 
 
         }
+    }
+
+    private fun retireveAndSetUsername() {
+        val userRef = dbRef.child("users").child(auth.currentUser!!.uid)
+        userRef.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val name = snapshot.child("name").getValue(String::class.java)
+
+                if(name != null){
+                    setUsername(name)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    private fun setUsername(username: String){
+        binding.tvUserName.text = username
+
+        val userInitial = requireActivity().findViewById<TextView>(R.id.user_initial)
+        userInitial.text = username[0].uppercase()
     }
 
     override fun onDestroyView() {
